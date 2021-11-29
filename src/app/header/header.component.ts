7e9be5531defb1876beb8acf4cc9d8bde3a5dc96
Hubert Orzełowski <s20467@pja.edu.pl>
2021-11-29 19:15:07 +0100

    Add header and footer components

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  isMenuOpen = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleOpen(){
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log(this.isMenuOpen);
+  }
+
+}
