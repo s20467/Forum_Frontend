e293f29b5cd002097cee1809f6e767fbe915a821
Hubert Orzełowski <s20467@pja.edu.pl>
2021-12-07 20:59:08 +0100

    Make user create real component from html/css stub

--- src/app/users/user-create/user-create.component.ts
+++ src/app/users/user-create/user-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/shared/user.model';
+import { usernameUsedValidator } from 'src/app/shared/username.validators';
+import { UsersService } from 'src/app/shared/users.service';
 
 @Component({
   selector: 'app-user-create',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCreateComponent implements OnInit {
 
-  constructor() { }
+  userCreateForm: FormGroup;
+
+  constructor(private route: ActivatedRoute, private usersService: UsersService, private router: Router) { }
 
   ngOnInit(): void {
+    this.userCreateForm = new FormGroup({
+      'username': new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(20)], usernameUsedValidator(this.usersService, null)),
+      'new-password': new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+      'new-password-repeat': new FormControl(null)
+    }, this.passwordRepeatMatchValidator);
+  }
+
+  onSubmit(){
+    if(this.userCreateForm.valid){
+      let newUser: User = new User();
+      newUser.username = this.userCreateForm.value['username'];
+      newUser.password = this.userCreateForm.value['new-password'];
+      this.usersService.createUser(newUser).subscribe(() => {
+        this.usersService.emitUsersChanged();
+        this.router.navigate(['../list'], {relativeTo: this.route});
+      })
+    }
+  }
+
+  passwordRepeatMatchValidator(group: FormGroup): ValidationErrors{
+    if(group.controls['new-password'].value !== group.controls['new-password-repeat'].value){
+      group.controls['new-password-repeat'].setErrors({passwordRepeatMatch: true});
+      return {passwordRepeatMatch: true};
+    }
+    return null;
   }
 
 }
