2be78c7fdfce6f62a9d7ea43d75aa4fb811b80a4
Hubert Orzełowski <s20467@pja.edu.pl>
2021-12-07 20:43:57 +0100

    Modify lock-button
    Modified lock button to make it more interactive. Made it possible to bind
    isLocked property which controls if there should be open or close padlock icon

--- src/app/shared/buttons/lock-button/lock-button.component.ts
+++ src/app/shared/buttons/lock-button/lock-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lock-button',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lock-button.component.css']
 })
 export class LockButtonComponent implements OnInit {
+
+  @Input('is-locked') isLocked=false;
 
   constructor() { }
 
