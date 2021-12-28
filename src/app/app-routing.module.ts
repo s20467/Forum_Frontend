7d935ca838cc45d41e4a83c3dc70c966c0f5ddec
Hubert Orzełowski <s20467@pja.edu.pl>
2021-12-28 15:35:00 +0100

    Fix routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllQuestionsComponent } from './questions/all-questions/all-questions.component';
+import { OpenQuestionsComponent } from './questions/open-questions/open-questions.component';
 import { AnswerEditComponent } from './questions/question-details/answers/answer-edit/answer-edit.component';
 import { AnswerListComponent } from './questions/question-details/answers/answer-list/answer-list.component';
 import { AnswersComponent } from './questions/question-details/answers/answers.component';
 import { QuestionDetailsComponent } from './questions/question-details/question-details.component';
 import { QuestionEditComponent } from './questions/question-edit/question-edit.component';
-import { QuestionListComponent } from './questions/question-list/question-list.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { UserQuestionsComponent } from './questions/user-questions/user-questions.component';
 import { LoginComponent } from './users/login/login.component';
 import { UserChangePasswordComponent } from './users/user-change-password/user-change-password.component';
 import { UserCreateComponent } from './users/user-create/user-create.component';
@@ -14,11 +16,13 @@ import { UserListComponent } from './users/user-list/user-list.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/questions', pathMatch: 'full'},
+  {path: '', redirectTo: '/questions/all', pathMatch: 'full'},
   {path: 'questions', component: QuestionsComponent, children:[
     {path: 'new', component: QuestionEditComponent},
-    {path: 'list', component: QuestionListComponent},
-    {path: '', redirectTo: 'list', pathMatch: 'full'},
+    {path: 'all', component: AllQuestionsComponent},
+    {path: 'open', component: OpenQuestionsComponent},
+    {path: 'user-questions/:username', component: UserQuestionsComponent},
+    {path: '', redirectTo: 'all', pathMatch: 'full'},
     {path: ':questionId/edit', component: QuestionEditComponent},
     {path: ':questionId/answers', component: QuestionDetailsComponent, children:[
       {path: '', component: AnswersComponent, children:[
@@ -26,7 +30,8 @@ const routes: Routes = [
         {path: 'new', component: AnswerEditComponent},
         {path: ':answerId/edit', component: AnswerEditComponent}
       ]}
-    ]}
+    ]},
+    {path: ':questionId', redirectTo: ':questionId/answers'}
   ]},
   {path: 'users', component: UsersComponent, children:[
     {path: '', redirectTo: 'list', pathMatch: 'full'},
